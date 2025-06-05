f4d6d9256a425b2b3edcc845da8e97004c57ea2d
e4riya <kotkovdima0@gmail.com>
2025-06-05 22:14:59 +0300

    add folder shemas

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,4 +1,4 @@
-package hexlet.code;
+package hexlet.code.schemas;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -6,13 +6,13 @@ import java.util.function.Predicate;
 
 public class MapSchema<L, R> extends BaseSchema<Map<L, R>> {
     private final Map<String, Predicate<Map<L, R>>> predicates = new HashMap<>();
-    private Map<L, BaseSchema<Object>> shemas;
+    private Map<L, BaseSchema<R>> shemas;
 
-    public MapSchema shape(Map<L, BaseSchema<Object>> newShemas) {
+    public MapSchema shape(Map<L, BaseSchema<R>> newShemas) {
         this.shemas = newShemas;
         predicates.put(
             "shape", value -> {
-                for (Map.Entry<L, BaseSchema<Object>> entry : shemas.entrySet()) {
+                for (Map.Entry<L, BaseSchema<R>> entry : shemas.entrySet()) {
                     if (!entry.getValue().isValid(value.get(entry.getKey()))) {
                         return false;
                     }
